53b1f188f210af184d1561ded84c2e95e0fc491d
heokyeongmin390 <rudals7445@gamil.com>
2026-07-30 11:40:43 +0900

    YL-55 Feat(navigation): 네비게이션 및 헤더 추가 #10

---
+++ src/components/common/navigation/AppNavigation.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="navigation-wrapper">
+    <!-- 데스크톱 전용 사이드바 (화면 폭 768px 이상일 때 노출) -->
+    <DesktopSidebar
+      v-if="!isMobile"
+      :items="items"
+      :current-theme="currentTheme"
+      @on-camera-click="handleCameraClick"
+      @change-theme="updateTheme"
+    />
+
+    <!-- 모바일 전용 하단 네비게이션 (화면 폭 768px 미만일 때 노출) -->
+    <MobileBottomNav
+      v-else
+      :items="items"
+      :current-theme="currentTheme"
+      @on-camera-click="handleCameraClick"
+      @change-theme="updateTheme"
+    />
+  </div>
+</template>
+
+<script>
+import DesktopSidebar from './DesktopSidebar.vue'
+import MobileBottomNav from './MobileBottomNav.vue'
+import { navigationItems } from './navigationItems.js'
+
+export default {
+  name: 'AppNavigation',
+  components: {
+    DesktopSidebar,
+    MobileBottomNav,
+  },
+  data() {
+    return {
+      items: navigationItems,
+      isMobile: false,
+      currentTheme: 1,
+    }
+  },
+  mounted() {
+    this.checkMediaQuery()
+    window.addEventListener('resize', this.checkMediaQuery)
+  },
+  beforeUnmount() {
+    window.removeEventListener('resize', this.checkMediaQuery)
+  },
+  methods: {
+    checkMediaQuery() {
+      this.isMobile = window.innerWidth < 768
+    },
+    updateTheme(newTheme) {
+      this.currentTheme = newTheme
+    },
+    handleCameraClick() {
+      this.$emit('camera-triggered')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.navigation-wrapper {
+  display: contents;
+}
+</style>
---
+++ src/components/common/navigation/navigationItems.js
@@ -0,0 +1,28 @@
+export const navigationItems = [
+  {
+    name: '홈',
+    path: '/',
+    icon: 'home',
+  },
+  {
+    name: '캘린더',
+    path: '/round',
+    icon: 'calendar',
+  },
+  {
+    name: '인증',
+    path: '/feed/write',
+    icon: 'camera',
+    isHighlight: true,
+  },
+  {
+    name: '자산',
+    path: '/settlement',
+    icon: 'wallet',
+  },
+  {
+    name: '마이',
+    path: '/mypage',
+    icon: 'user',
+  },
+]
