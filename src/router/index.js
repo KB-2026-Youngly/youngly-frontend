866ccc351d784ff9b9c6dce2146b66b4eae25218
heokyeongmin390 <rudals7445@gamil.com>
2026-08-04 17:46:16 +0900

    YL-75 Feat: 로그인·회원가입 화면 및 인증 연동 구현 #14

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,25 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
+    redirect: '/login',
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/auth/LoginView.vue'),
+  },
+  {
+    path: '/signup/terms',
+    name: 'SignupTerms',
+    component: () => import('@/views/auth/SignupTermsView.vue'),
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: () => import('@/views/auth/SignupView.vue'),
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: () => import('@/views/home/HomeView.vue'),
   },
@@ -11,6 +30,7 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior: () => ({ top: 0 }),
 })
 
 export default router
