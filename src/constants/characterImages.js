a7acc63021a112f24130f8132e6d5e770c2a152f
신성욱 <cow9708@gmail.com>
2026-08-20 18:07:04 +0900

    캐릭터 추가

--- src/constants/characterImages.js
+++ src/constants/characterImages.js
@@ -22,6 +22,14 @@ import bibi05Image from '@/assets/characters/bibi-05.png'
 import bibi06Image from '@/assets/characters/bibi-06.png'
 import bibi07Image from '@/assets/characters/bibi-07.png'
 import bibi08Image from '@/assets/characters/bibi-08.png'
+import colli01Image from '@/assets/characters/colli-01.png'
+import colli02Image from '@/assets/characters/colli-02.png'
+import colli03Image from '@/assets/characters/colli-03.png'
+import colli04Image from '@/assets/characters/colli-04.png'
+import colli05Image from '@/assets/characters/colli-05.png'
+import colli06Image from '@/assets/characters/colli-06.png'
+import colli07Image from '@/assets/characters/colli-07.png'
+import colli08Image from '@/assets/characters/colli-08.png'
 
 const CHARACTER_IMAGE_BY_FILE_NAME = Object.freeze({
   'kiki-01.png': kiki01Image,
@@ -48,6 +56,14 @@ const CHARACTER_IMAGE_BY_FILE_NAME = Object.freeze({
   'bibi-06.png': bibi06Image,
   'bibi-07.png': bibi07Image,
   'bibi-08.png': bibi08Image,
+  'colli-01.png': colli01Image,
+  'colli-02.png': colli02Image,
+  'colli-03.png': colli03Image,
+  'colli-04.png': colli04Image,
+  'colli-05.png': colli05Image,
+  'colli-06.png': colli06Image,
+  'colli-07.png': colli07Image,
+  'colli-08.png': colli08Image,
 })
 
 const CHARACTER_DISPLAY_NAME_BY_FILE_NAME = Object.freeze({
@@ -67,6 +83,12 @@ const CHARACTER_DISPLAY_NAME_BY_FILE_NAME = Object.freeze({
     Array.from({ length: 8 }, (_, index) => [
       `bibi-${String(index + 1).padStart(2, '0')}.png`,
       '비비',
+    ]),
+  ),
+  ...Object.fromEntries(
+    Array.from({ length: 8 }, (_, index) => [
+      `colli-${String(index + 1).padStart(2, '0')}.png`,
+      '콜리',
     ]),
   ),
 })
